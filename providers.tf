0925efc73ca09a45afec68ab078e1a7b64ce8db2
winsletvasanthraaj <91631588+winsletvasanthraaj@users.noreply.github.com>
2022-03-30 17:45:37 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -16,3 +16,12 @@ terraform {
 provider "azurerm" {
   features {}
 }
+provider"azuread" {
+
+client_id = "637266ad-6607-4ad3-87ed-6b16f8e24548"
+
+client_secret = var.client_secret
+
+tenant_id = "0354a672-9919-4a99-bfc6-e760f06753ee"
+
+}
